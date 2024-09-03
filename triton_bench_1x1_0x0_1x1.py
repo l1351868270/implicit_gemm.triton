d060534cd61cdbfb19f5246caea6699289eaf168
root <root@DESKTOP-9J3FKR0>
2024-09-03 14:19:29 +0800

    1x1_0x0_1x1

---
+++ triton_bench_1x1_0x0_1x1.py
@@ -0,0 +1,218 @@
+import torch
+import triton
+import triton.language as tl
+
+from triton_implicit_gemm_1x1_0x0_1x1 import triton_implicit_gemm_1x1_0x0_1x1
+from triton_gemm import matmul
+
+ref_lib = 'cuBLAS'
+configs = []
+configs.append(
+    triton.testing.Benchmark(
+        x_names=['x_name', 'N', 'C', 'H', 'W', 'K', 'R', 'S', 'U', 'V', 'pad_h', 'pad_w', 'dila_h', 'dila_w'],
+        x_vals=[('conv1', 1, 3, 224, 224, 64, 7, 7, 2, 2, 3, 3, 1, 1), 
+            ('layer1.0.conv1', 1, 64, 56, 56, 64, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.0.conv2', 1, 64, 56, 56, 64, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer1.0.conv3', 1, 64, 56, 56, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.0.downsample', 1, 64, 56, 56, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.1.conv1', 1, 256, 56, 56, 64, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.1.conv2', 1, 64, 56, 56, 64, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer1.1.conv3', 1, 64, 56, 56, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.2.conv1', 1, 256, 56, 56, 64, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer1.2.conv2', 1, 64, 56, 56, 64, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer1.2.conv3', 1, 64, 56, 56, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.0.conv1', 1, 256, 56, 56, 128, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.0.conv2', 1, 128, 28, 28, 128, 3, 3, 2, 2, 1, 1, 1, 1),
+            ('layer2.0.conv3', 1, 128, 28, 28, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.0.downsample', 1, 256, 28, 28, 512, 1, 1, 2, 2, 0, 0, 1, 1),
+            ('layer2.1.conv1', 1, 512, 28, 28, 128, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.1.conv2', 1, 128, 28, 28, 128, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer2.1.conv3', 1, 128, 28, 28, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.2.conv1', 1, 512, 28, 28, 128, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.2.conv2', 1, 128, 28, 28, 128, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer2.2.conv3', 1, 128, 28, 28, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.3.conv1', 1, 512, 28, 28, 128, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer2.3.conv2', 1, 128, 28, 28, 128, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer2.3.conv3', 1, 128, 28, 28, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.0.conv1', 1, 512, 28, 28, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.0.conv2', 1, 256, 14, 14, 256, 3, 3, 2, 2, 1, 1, 1, 1),
+            ('layer3.0.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.0.downsample', 1, 512, 14, 14, 1024, 1, 1, 2, 2, 0, 0, 1, 1),
+            ('layer3.1.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.1.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.1.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.2.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.2.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.2.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.3.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.3.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.3.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.4.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.4.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.4.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.5.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.5.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.5.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.6.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.6.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.6.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.7.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.7.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.7.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.8.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.8.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.8.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.9.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.9.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.9.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.10.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.10.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.10.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.11.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.11.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.11.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.12.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.12.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.12.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.13.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.13.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.13.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.14.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.14.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.14.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.15.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.15.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.15.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.15.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.15.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.15.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.16.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.16.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.16.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.17.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.17.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.17.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.18.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.18.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.18.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.19.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.19.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.19.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.20.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.20.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.20.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.21.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.21.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.21.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.22.conv1', 1, 1024, 14, 14, 256, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer3.22.conv2', 1, 256, 14, 14, 256, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer3.22.conv3', 1, 256, 14, 14, 1024, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer4.0.conv1', 1, 1024, 14, 14, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer4.0.conv2', 1, 512, 7, 7, 512, 3, 3, 2, 2, 1, 1, 1, 1),
+            ('layer4.0.conv3', 1, 512, 7, 7, 2048, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer4.0.downsample', 1, 1024, 7, 7, 2048, 1, 1, 2, 2, 0, 0, 1, 1),
+            ('layer4.1.conv1', 1, 2048, 7, 7, 512, 1, 1, 1, 1, 0, 0, 1, 1),
+            ('layer4.1.conv2', 1, 512, 7, 7, 512, 3, 3, 1, 1, 1, 1, 1, 1),
+            ('layer4.1.conv3', 1, 512, 7, 7, 2048, 1, 1, 1, 1, 0, 0, 1, 1),
+            ],
+        line_arg="provider",  # Argument name whose value corresponds to a different line in the plot
+        # Possible values for `line_arg`
+        # Don't compare to cublas for fp8 cases as torch.matmul doesn't support fp8 at the moment.
+        line_vals=[ref_lib.lower(), "triton_implicit_gemm", 'torch_gemm_tn', 'triton_gemm_tn'],  # Label name for the lines
+        line_names=[ref_lib, "Triton_implicit_gemm", 'Torch_gemm_tn', 'Triton_gemm_tn'],  # Line styles
+        styles=[("green", "-"), ("blue", "-"), ("red", "-"), ("gray", "-")],  # Line color and style
+        ylabel="TFLOPS",  # Label name for the y-axis
+        plot_name="matmul-performance-fp16",
+        args={},
+    ))
+@triton.testing.perf_report(configs)
+def benchmark(x_name, N, C, H, W, K, R, S, U, V, pad_h, pad_w, dila_h, dila_w, provider):
+    P = (H + 2 * pad_h - dila_h * (R - 1) - 1) // U + 1
+    Q = (W + 2 * pad_w - dila_w * (S - 1) - 1) // V + 1
+    GEMM_M = N * P * Q
+    GEMM_N = K
+    GEMM_K = C * R * S
+    gflops = (2.0*GEMM_M*GEMM_N*GEMM_K) * 1e-9
+
+    x = torch.randn(N, C, H, W).cuda().half()
+    w = torch.randn(K, C, R, S).cuda().half()
+    conv2d = torch.nn.Conv2d(C, K, (R, S), stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w), bias=False).cuda().half()
+    conv2d.weight.data = w
+    conv2d = conv2d.to(memory_format=torch.channels_last)
+    x = x.to(memory_format=torch.channels_last)
+    w = w.to(memory_format=torch.channels_last)
+
+    if provider == ref_lib.lower():
+        ms = triton.testing.do_bench(lambda: conv2d(x))
+
+    if provider == 'triton_implicit_gemm':
+        if U == 1 and V == 1 and pad_h == 0 and pad_w == 0 and dila_h == 1 and dila_w == 1:
+            y_torch = conv2d(x)
+            y_triton = triton_implicit_gemm_1x1_0x0_1x1(x, w, stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w))
+            if not torch.allclose(y_torch, y_triton, atol=1e-2, rtol=1e-2):
+                print("Torch and triton_implicit_gemm differ")
+                print(f'C:{C}, H:{H}, W:{W}, K:{K}, P:{P}, Q:{Q}, U:{U}, V:{V}, pad_h:{pad_h}, pad_w:{pad_w}, dila_h:{dila_h}, dila_w:{dila_w}')
+            ms = triton.testing.do_bench(lambda: triton_implicit_gemm_1x1_0x0_1x1(x, w, stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w)))
+        else:
+            ms = float('inf')
+
+    if provider == 'torch_gemm_tn':
+        gemm_a = torch.randn(GEMM_M, GEMM_K).cuda().half()
+        gemm_b = torch.randn(GEMM_N, GEMM_K).cuda().half().T
+        ms = triton.testing.do_bench(lambda: torch.matmul(gemm_a, gemm_b)) 
+
+    if provider == 'triton_gemm_tn':
+        gemm_a = torch.randn(GEMM_M, GEMM_K).cuda().half()
+        gemm_b = torch.randn(GEMM_N, GEMM_K).cuda().half().T
+        ms = triton.testing.do_bench(lambda: matmul(gemm_a, gemm_b))
+
+    perf = lambda ms: gflops / ms
+    return perf(ms)
+
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+    N = 1; C = 64; H = 56; W = 56; K = 64; R = 1; S = 1; pad_h = 0; pad_w = 0; U = 1; V = 1; dila_h = 1; dila_w = 1
+    # N = 1; C = 1; H = 3; W = 3; K = 1; R = 3; S = 3; pad_h = 0; pad_w = 0; U = 1; V = 1; dila_h = 1; dila_w = 1
+
+    x = torch.randn(N, C, H, W).cuda().half()
+    w = torch.randn(K, C, R, S).cuda().half()
+    conv2d = torch.nn.Conv2d(C, K, (R, S), stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w), bias=False).cuda().half()
+    conv2d.weight.data = w
+    y1 = conv2d(x)
+
+    w = w.to(memory_format=torch.channels_last)
+    x = x.to(memory_format=torch.channels_last)
+    conv2d = conv2d.to(memory_format=torch.channels_last)
+    conv2d.weight.data = w
+    y2 = conv2d(x)
+
+    y3 = triton_implicit_gemm_1x1_0x0_1x1(x, w, stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w))
+    
+    if torch.allclose(y1, y3, atol=1e-2, rtol=1e-2):
+        print("Torch and triton_implicit_gemm match")
+    else:
+        print("Torch and triton_implicit_gemm differ")
+        print(f'torch: shape:{y1.shape}, stride:{y1.stride()}, {y1}')
+        print(f'triton_implicit_gemm: shape:{y3.shape}, stride:{y3.stride()}, {y3}')
+    
+    P = (H + 2 * pad_h - dila_h * (R - 1) - 1) // U + 1
+    Q = (W + 2 * pad_w - dila_w * (S - 1) - 1) // V + 1
+    GEMM_M = N * P * Q
+    GEMM_K = R * S * C
+    GEMM_N = K
+
+    gemm_a = torch.randn(GEMM_M, GEMM_K).cuda().half()
+    gemm_b = torch.randn(GEMM_N, GEMM_K).cuda().half().T
+    gemm_c = matmul(gemm_a, gemm_b)
+
+    gemm_c1 = torch.matmul(gemm_a, gemm_b)
+
+    if torch.allclose(gemm_c1, gemm_c, atol=1e-2, rtol=1e-2):
+        print("gemm: Torch and Triton match")
+    else:
+        print("Torch and Triton differ")
+        print(f'gemm torch: shape:{gemm_c1.shape}, stride:{gemm_c1.stride()}, {gemm_c1}')
+        print(f'gemm triton: shape:{gemm_c.shape}, stride:{gemm_c.stride()}, {gemm_c}')
+        
+    benchmark.run(show_plots=False, print_data=True)
---
+++ triton_implicit_gemm_1x1_0x0_1x1.py
@@ -0,0 +1,190 @@
+import torch
+import triton
+import triton.language as tl
+import triton.ops
+
+def get_autotune_config():
+    return [
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=3,
+        #               num_warps=8),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+                      num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+        #               num_warps=2),
+        # triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+        #               num_warps=2),
+        # triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=5,
+        #               num_warps=1),
+        # # Good config for fp8 inputs.
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+        #               num_warps=8),
+        # triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=3,
+        #               num_warps=8),
+        # triton.Config({'BLOCK_SIZE_M': 256, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=4,
+        #               num_warps=4)
+    ]
+
+@triton.autotune(
+    configs=get_autotune_config(),
+    key=['GEMM_M', 'GEMM_N', 'GEMM_K'],
+)
+@triton.jit
+def conv2d_kernel_1x1_1x1_0x0_1x1(
+        x_ptr, w_ptr, y_ptr,
+        N, C, H, W, K, P, Q, R, S, U, V, pad_h, pad_w, dila_h, dila_w, 
+        GEMM_M, GEMM_N, GEMM_K,
+        stride_am, stride_ak,  #
+        stride_bk, stride_bn,  #
+        stride_cm, stride_cn,
+        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,  #
+        GROUP_SIZE_M: tl.constexpr,  #
+):
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(GEMM_M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(GEMM_N, BLOCK_SIZE_N)
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
+
+    gemm_i = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % GEMM_M
+    gemm_j = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % GEMM_N
+
+    n = gemm_i // (P * Q)
+    npq_residual = gemm_i % (P * Q)
+    p = npq_residual // Q
+    q = npq_residual % Q
+    k = gemm_j
+
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % GEMM_M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % GEMM_N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = x_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b_ptrs = w_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for idx_k in range(0, tl.cdiv(GEMM_K, BLOCK_SIZE_K)):
+
+        gemm_k = (idx_k * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)) # % GEMM_K
+        r = gemm_k // (S * C)
+        rsc_residual = gemm_k % (S * C)
+        s = rsc_residual // C
+        c = rsc_residual % C
+        h = p[:, None] * U + r[None, :] * dila_h - pad_h
+        w = q[:, None] * V + s[None, :] * dila_w - pad_w
+        mask_x = (h >= 0) & (h < H) & (w >= 0) & (w < W)
+        mask_w = (r < R) & (s < S) & (c < C)
+        offs_x = n[:, None] * H * W * C + h * W * C + w * C + c
+        offs_w = k[None, :] * R * S * C + r[:, None] * S * C + s[:, None] * C + c[:, None]
+    
+        x_ptrs = x_ptr + offs_x
+        w_ptrs = w_ptr + offs_w
+
+        # a = tl.load(x_ptrs)
+        # b = tl.load(w_ptrs)
+        a = tl.load(a_ptrs)
+        b = tl.load(b_ptrs)
+        accumulator = tl.dot(a, b, accumulator, out_dtype=tl.float32)
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+
+    c = accumulator.to(tl.float16)
+
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = y_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < GEMM_M) & (offs_cn[None, :] < GEMM_N)
+    tl.store(c_ptrs, c, mask=c_mask)
+
+def triton_implicit_gemm_1x1_0x0_1x1(x: torch.Tensor, w: torch.Tensor, stride=(1, 1), padding=(0, 0), dilation=(1, 1)):
+    # Check constraints.
+    assert x.shape[1] == w.shape[1], "Incompatible dimensions"
+
+    N, C, H, W = x.shape
+    K, C, R, S = w.shape
+    U, V = stride
+    pad_h, pad_w = padding
+    dila_h, dila_w = dilation
+    P = (H + 2 * pad_h - dila_h * (R - 1) - 1) // U + 1
+    Q = (W + 2 * pad_w - dila_w * (S - 1) - 1) // V + 1
+    # y = torch.empty(N, K, P, Q, device=x.device, dtype=torch.float16).to(memory_format=torch.channels_last)
+    
+    GEMM_M = N * P * Q
+    GEMM_N = K
+    GEMM_K = C * R * S
+    y = torch.empty((N, K, P, Q), device=x.device, dtype=torch.float16, memory_format=torch.channels_last)
+    # 1D launch kernel where each block gets its own program.
+    grid = lambda META: (triton.cdiv(GEMM_M, META['BLOCK_SIZE_M']) * triton.cdiv(GEMM_N, META['BLOCK_SIZE_N']), )
+    conv2d_kernel_1x1_1x1_0x0_1x1[grid](
+        x, w, y,  #
+        N, C, H, W, K, P, Q, R, S, U, V, pad_h, pad_w, dila_h, dila_w, 
+        GEMM_M, GEMM_N, GEMM_K,  #
+        GEMM_K, 1,  #
+        1, GEMM_K,  #
+        GEMM_N, 1,  #
+    )
+    return y
+
+def triton_implicit_gemm_1x1_0x0_1x1_(x: torch.Tensor, w: torch.Tensor, stride=(1, 1), padding=(0, 0), dilation=(1, 1)):
+    N, C, H, W = x.shape
+    K, C, R, S = w.shape
+    U, V = stride
+    pad_h, pad_w = padding
+    dila_h, dila_w = dilation
+    P = (H + 2 * pad_h - dila_h * (R - 1) - 1) // U + 1
+    Q = (W + 2 * pad_w - dila_w * (S - 1) - 1) // V + 1
+    y = torch.empty((N, K, P, Q), device=x.device, dtype=torch.float16).to(memory_format=torch.channels_last)
+    GEMM_M = N * P * Q
+    GEMM_N = K
+    GEMM_K = C * R * S
+    grid = lambda META: (triton.cdiv(GEMM_M, META['BLOCK_SIZE_M']) * triton.cdiv(GEMM_N, META['BLOCK_SIZE_N']), )
+    conv2d_kernel_1x1_1x1_0x0_1x1[grid](x, w, y, N, C, H, W, K, P, Q, R, S, U, V, pad_h, pad_w, dila_h, dila_w, GEMM_M, GEMM_N, GEMM_K)
+    return y
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+    N = 1; C = 128; H = 56; W = 56; K = 64; R = 1; S = 1; pad_h = 0; pad_w = 0; U = 1; V = 1; dila_h = 1; dila_w = 1
+    # N = 1; C = 1; H = 3; W = 3; K = 1; R = 3; S = 3; pad_h = 0; pad_w = 0; U = 1; V = 1; dila_h = 1; dila_w = 1
+
+    x = torch.randn(N, C, H, W).cuda().half()
+    w = torch.randn(K, C, R, S).cuda().half()
+    conv2d = torch.nn.Conv2d(C, K, (R, S), stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w), bias=False).cuda().half()
+    conv2d.weight.data = w
+    y1 = conv2d(x)
+
+    w = w.to(memory_format=torch.channels_last)
+    x = x.to(memory_format=torch.channels_last)
+    conv2d = conv2d.to(memory_format=torch.channels_last)
+    conv2d.weight.data = w
+    y2 = conv2d(x)
+
+    y3 = triton_implicit_gemm_1x1_0x0_1x1(x, w, stride=(U, V), padding=(pad_h, pad_w), dilation=(dila_h, dila_w))
+    
+    if torch.allclose(y1, y3, atol=1e-2, rtol=1e-2):
+        print("Torch and triton_implicit_gemm match")
+    else:
+        print("Torch and triton_implicit_gemm differ")
+        print(f'torch: shape:{y1.shape}, stride:{y1.stride()}, {y1}')
+        print(f'triton_implicit_gemm: shape:{y3.shape}, stride:{y3.stride()}, {y3}')
+    
+
